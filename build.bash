2351a46782733f1e3a64f25c01d0798d3d40b46c
ashn <me@ashn.dev>
2020-01-04 13:43:20 -0500

    Dynamically generate blog index
    The build.bash script now automatically generates the href="/blog.html"
    page using the list of blog entries under the src/blog/ directory
    instead of requiring each blog entry to be added to the page by hand.
    
    Since there is currently only one blog entry I am not sure if the script
    will work for entries > 1, so I further updates may be required.

--- build.bash
+++ build.bash
@@ -2,24 +2,29 @@
 
 set -e
 
+NL=$'\n'
 SRC_DIR='src'
 OUT_DIR='out'
 
-#== Create Output Directory
+#== Create Output Directory ====================================================
 if [ -d "${OUT_DIR}" ]; then
     rm -rf "${OUT_DIR}"
 fi
 mkdir -p "${OUT_DIR}"
 mkdir -p "${OUT_DIR}/blog"
 
-#== Generate Website
+#== Website Generation Helper Utilities ========================================
 TEMPLATE=$(cat template.html)
+
+md_page_title() {
+    FILE="$1"
+    head -n 1 "${FILE}"
+}
 
 make_page() {
     PAGE_TITLE="$1"
     PAGE_CONTENT="$2"
 
-    # Non sh-compliant bash substitutions.
     PAGE_OUT="${TEMPLATE}"
     PAGE_OUT="${PAGE_OUT/<!--TITLE-->/${PAGE_TITLE}}"
     PAGE_OUT="${PAGE_OUT/<!--PAGE-->/${PAGE_CONTENT}}"
@@ -27,7 +32,7 @@ make_page() {
     echo "${PAGE_OUT}"
 }
 
-build_page_html() {
+build_page_from_html() {
     SRC_PATH="$1" # Path under the src/ directory.
     TITLE="$2"    # Page title.
     echo "BUILDING HTML PAGE: ${SRC_PATH}"
@@ -39,7 +44,7 @@ build_page_html() {
     make_page "${TITLE}" "${CONTENT}" > "${OUT_FILE}"
 }
 
-build_page_markdown() {
+build_page_from_md() {
     SRC_PATH="$1" # Path under the src/ directory.
     TITLE="$2"    # Page title.
     echo "BUILDING MARKDOWN PAGE: ${SRC_PATH}"
@@ -51,10 +56,40 @@ build_page_markdown() {
     make_page "${TITLE}" "${CONTENT}" > "${OUT_FILE}"
 }
 
+build_blog_page() {
+    SRC_PATH="$1" # Path under the src/ directory.
+
+    TITLE="$(md_page_title "${SRC_DIR}/${SRC_PATH}")"
+    build_page_from_md "${SRC_PATH}" "${TITLE}"
+}
+
+#== Generate the Actual Website ================================================
 cp "${SRC_DIR}/favicon.ico" "${OUT_DIR}"
 cp "${SRC_DIR}/style.css" "${OUT_DIR}"
 
-build_page_html "index.html" "ashn"
-build_page_html "blog.html" "blog"
+echo "==== BUILDING HTML PAGES ===="
+build_page_from_html "index.html" "ashn"
 
-build_page_markdown "blog/2020-01-01-hello-world.md" "Hello World"
+echo "==== BUILDING BLOG INDEX PAGE ===="
+INDEX_CONTENT=$(cat "${SRC_DIR}/blog.html")
+BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}<ul>"
+for f in $(ls -C "${SRC_DIR}/blog"); do
+    echo "PROCESSING BLOG ENTRY: ${f}"
+    # YYYY-MM-DD
+    # 123456789A bytes should be parsed to get the date from the filename.
+    F_DATE=$(echo "${f}" | head -c 10 -)
+    F_TITLE=$(md_page_title "${SRC_DIR}/blog/${f}")
+    F_HREF="/blog/${f%.md}.html"
+
+    BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}<li>"
+    BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}  ${F_DATE} ⟶ "
+    BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}  <a href=\"${F_HREF}\">"
+    BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}    ${F_TITLE}"
+    BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}  </a>"
+    BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}</li>"
+done
+BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}</ul>"
+make_page "blog index" "${BLOG_INDEX_CONTENT}" > "${OUT_DIR}/blog.html"
+
+echo "==== BUILDING BLOG ENTRY PAGES ===="
+build_blog_page "blog/2020-01-01-hello-world.md"
