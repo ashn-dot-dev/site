7eea2d0f8e6f4eedafa210b26ff486ca09fad78d
ashn <me@ashn.dev>
2020-01-07 17:54:49 -0500

    Remove non-ASCII arrow to improve accessibility
    The Unicode arrow that was previously being used in the generated
    blog.html page did not render correctly in the Opera Mini browser.
    This commit changes the format of the generated HTML to use ASCII
    instead of the unsupported code point.

--- build.bash
+++ build.bash
@@ -82,7 +82,7 @@ for f in $(ls "${SRC_DIR}/blog" | sort -r); do
     F_HREF="/blog/${f%.md}.html"
 
     BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}<li>"
-    BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}  ${F_DATE} ⟶ "
+    BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}  [ ${F_DATE} ] "
     BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}  <a href=\"${F_HREF}\">"
     BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}    ${F_TITLE}"
     BLOG_INDEX_CONTENT="${BLOG_INDEX_CONTENT}${NL}  </a>"
