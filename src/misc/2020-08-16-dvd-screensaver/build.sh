0da4a12074b584b47fed2cf89f915cba28771738
ashn <me@ashn.dev>
2023-07-10 21:12:31 -0400

    Summer cleaning

--- src/misc/2020-08-16-dvd-screensaver/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# The default version of g++ shipping with Ubuntu 16.04 (g++-7) will produce a
-# compilation error since libstdc++fs is still marked as experimental for C++17
-# code.
-# Running:
-#   $ sudo apt-get install g++-8
-# will give you g++-8, which does support the C++17 filesystem API.
-g++-8 -o dvd -std=c++17 main.cpp -lX11 -lGL -lpthread -lpng -lstdc++fs
